da1abe9c81adea8d80f2b9a47699e46dc6b288bb
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-08-27 16:54:05 +0000

    Add: Project path organize

--- tournments/apps.py
+++ tournments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppsystemConfig(AppConfig):
+class TournmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appsystem'
+    name = 'tournments'
