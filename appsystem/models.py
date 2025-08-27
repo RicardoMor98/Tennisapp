5d8114f25e232500f8d81366b8edf970d6d6f764
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-08-27 15:10:53 +0000

    Add: Create image upload and Player profile model

--- appsystem/models.py
+++ appsystem/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db import models  
+from django.contrib.auth.models import User  
+from django.core.validators import MinValueValidator, MaxValueValidator  
+import os  
+
 
 # Create your models here.
+def user_image_path(instance, filename):  
+    # Upload to media/user_<id>/profile_<filename>  
+    return f'user_{instance.user.id}/profile_{filename}'
+
+class PlayerProfile(models.Model):  
+    SKILL_LEVELS = [  
+        ('beginner', 'Beginner'),  
+        ('intermediate', 'Intermediate'),  
+        ('advanced', 'Advanced'),  
+        ('competition', 'Competition'),  
+    ]  
