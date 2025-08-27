2fce19f35826b169c0a61988ecf92776426b37c7
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2025-08-27 17:07:58 +0000

    Add: Create Profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .models import PlayerProfile
+from .serializers import PlayerProfileSerializer, PlayerProfileDetailSerializer
+from tennisapp.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+class PlayerProfileList(generics.ListAPIView):
+    queryset = PlayerProfile.objects.all()
+    serializer_class = PlayerProfileSerializer
+
+class PlayerProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    queryset = PlayerProfile.objects.all()
+    serializer_class = PlayerProfileDetailSerializer
+    
+    def get_object(self):
+        # Allow users to access their own profile by ID or by 'me'
+        if self.kwargs['pk'] == 'me':
+            return self.request.user.playerprofile
+        return super().get_object()
